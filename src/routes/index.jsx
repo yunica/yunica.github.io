061efd43c66844769118371c17607aee0cd7c554
Junior flores <junior@developmentseed.org>
2020-01-02 12:55:58 -0500

    Integrate template

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,5 @@
-import Components from "views/Components/Components.jsx";
-import LandingPage from "views/LandingPage/LandingPage.jsx";
-import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
-import LoginPage from "views/LoginPage/LoginPage.jsx";
+import ProfilePage from "../views/ProfilePage/ProfilePage";
 
-var indexRoutes = [
-  { path: "/landing-page", name: "LandingPage", component: LandingPage },
-  { path: "/profile-page", name: "ProfilePage", component: LoginPage },
-  { path: "/ejemplos", name: "Components", component: Components },
-  { path: "/", name: "LoginPage", component: ProfilePage }
-];
+var indexRoutes = [{ path: "/", name: "ProfilePage", component: ProfilePage }];
 
 export default indexRoutes;
