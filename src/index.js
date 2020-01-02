d578e4b6c8c91b57f487fc7f05d4ba8c8e6a2573
Junior flores <junior@developmentseed.org>
2020-01-02 14:12:33 -0500

    changes for deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-import indexRoutes from "routes/index.jsx";
+import indexRoutes from "routes/index";
 
 import "assets/css/material-kit-react.css";
 
