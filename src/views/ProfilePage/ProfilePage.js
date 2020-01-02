9b0722d4e5b1156fb9d4274d8126c662e30128a1
Junior flores <junior@developmentseed.org>
2020-01-02 14:22:58 -0500

    Integrate gh pages

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -4,42 +4,19 @@ import React from "react";
 import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import Tooltip from "@material-ui/core/Tooltip";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
+
 // @material-ui/icons
-import Camera from "@material-ui/icons/Camera";
-import Palette from "@material-ui/icons/Palette";
-import People from "@material-ui/icons/People";
-import Add from "@material-ui/icons/Add";
-import Favorite from "@material-ui/icons/Favorite";
+
 // core components
-import Header from "../../components/Header/Header.js";
-import Footer from "../../components/Footer/Footer.js";
+
 import GridContainer from "../../components/Grid/GridContainer.js";
 import GridItem from "../../components/Grid/GridItem.js";
-import HeaderLinks from "../../components/Header/HeaderLinks.js";
-import NavPills from "../../components/NavPills/NavPills.js";
-import Card from "../../components/Card/Card.js";
-import CardBody from "../../components/Card/CardBody.js";
-import CardHeader from "../../components/Card/CardHeader.js";
-import Badge from "../../components/Badge/Badge.js";
-import Muted from "../../components/Typography/Muted.js";
+
 import Parallax from "../../components/Parallax/Parallax.js";
 import Clearfix from "../../components/Clearfix/Clearfix.js";
-import Button from "../../components/CustomButtons/Button.js";
 
 import christian from "assets/img/ju.jpeg";
-import oluEletu from "assets/img/examples/olu-eletu.jpg";
-import clemOnojeghuo from "assets/img/examples/mariya-georgieva.jpg";
-import cynthiaDelRio from "assets/img/examples/mariya-georgieva.jpg";
-import mariyaGeorgieva from "assets/img/examples/mariya-georgieva.jpg";
-import clemOnojegaw from "assets/img/examples/mariya-georgieva.jpg";
-import darrenColeshill from "assets/img/examples/mariya-georgieva.jpg";
-import avatar from "assets/img/faces/avatar.jpg";
-import marc from "assets/img/faces/marc.jpg";
-import kendall from "assets/img/faces/kendall.jpg";
-import cardProfile2Square from "assets/img/faces/card-profile2-square.jpg";
+
 
 import profilePageStyle from "assets/jss/material-kit-pro-react/views/profilePageStyle.js";
 
