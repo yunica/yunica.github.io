a04e126e04972532fae8c2e35826b5bd1c4e2ffa
Junior flores <junior@developmentseed.org>
2020-01-02 15:05:03 -0500

    Updates

---
+++ precache-manifest.58aa0addb6834659f67561807a9266a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36af5ae687853c6cbb51d9e85c273f49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "188e4a2c0f7284ce36b5",
+    "url": "/static/css/main.046439fd.chunk.css"
+  },
+  {
+    "revision": "b131333dd8bc80ac8d94",
+    "url": "/static/js/2.77d3fb20.chunk.js"
+  },
+  {
+    "revision": "188e4a2c0f7284ce36b5",
+    "url": "/static/js/main.dfa860a8.chunk.js"
+  },
+  {
+    "revision": "b3ddc7c185031b38312d",
+    "url": "/static/js/runtime~main.66db7f2f.js"
+  },
+  {
+    "revision": "e8a8b8e11cc48bfae0dd1c8ebb5b200d",
+    "url": "/static/media/building-webpage.e8a8b8e1.mp4"
+  },
+  {
+    "revision": "58a713c73886bac25ac6aedcbb13de5a",
+    "url": "/static/media/city.58a713c7.jpg"
+  },
+  {
+    "revision": "eabf7bd169d681cc2d0618ec56280258",
+    "url": "/static/media/ju.eabf7bd1.jpeg"
+  }
+]);
