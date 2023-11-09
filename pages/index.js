f016d13ce2d4cf221c9fa05ee2f490c0b85907f2
Junior flores <fmjg21@gmail.com>
2023-11-09 22:57:23 +0700

    clean index

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ function Main() {
           minWidth="700px"
           height="80%"
           position="absolute"
-          right="30px"
+          right="15px"
           top="5%"
           display={{ sm: "none", md: "block" }}
         >
@@ -31,17 +31,12 @@ function Main() {
           <Grid container spacing={0}>
             <Grid item xs={8} md={7} xl={7}>
               <Grid container spacing={2}>
-                <Grid item xs={12} md={6} xl={4}>
-                  <h1> daasdasd</h1>
-                  <h1> daasdasd</h1>
+                <Grid item xs={12} md={12} xl={12}>
+                  
                 </Grid>
-                <Grid item xs={12} md={6} xl={4}>
-                  <h1> daasdasd</h1>
-                  <h1> daasdasd</h1>
-                </Grid>
-                <Grid item xs={12} md={6} xl={4}>
-                  <h1> daasdasd</h1>
-                  <h1> daasdasd</h1>
+                <Grid item xs={12} md={12} xl={12}>
+                  <h2> Junior G. Flores Martinez</h2>
+                  <h3> Geospatial data engineer</h3>
                 </Grid>
               </Grid>
             </Grid>
